904deed20babe0d41c2cbf9a9dded34cd9f53621
Dipanshu Mahato <38698710+dipanshu-m@users.noreply.github.com>
2021-09-21 13:14:50 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,7 @@
+#Generates band name after taking input of home city name and pet name
+
+print('WELCOME TO THE BAND NAME GENERATOR')
+home_city= input("Enter the city you grew up on:\n")
+pet_name= input("Enter the name of your pet:\n")
+band_name=home_city+" "+pet_name
+print("The name of your band suggested is: "+band_name)
