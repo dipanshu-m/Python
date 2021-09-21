2550b5c8ceed2b272525e57456df0a76c5f62af2
Dipanshu Mahato <38698710+dipanshu-m@users.noreply.github.com>
2021-09-21 20:43:28 +0530

    Update TypeConvertion.py

--- notes/TypeConvertion.py
+++ notes/TypeConvertion.py
@@ -1,1 +1,11 @@
-#s
+x = 13.949999999999999999 #STEP 1: BY OVERFLOWING THE VALUE 
+print(type(x))
+print(x)
+
+g = float("{0:.2f}".format(x)) #STEP 2: CONVERTING THE VALUE WITHOUT CALLING THE ROUND FUNCTION
+print(g)
+print(x == g)
+
+h = round(x, 2) #STEP 3: CONVERTING THE VALUE BY CALLING THE ROUND FUNCTION
+print(h)
+print(x == h)
