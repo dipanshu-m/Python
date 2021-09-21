1e160e5733d215a8f94cbb765333d5facce5c45e
Dipanshu Mahato <38698710+dipanshu-m@users.noreply.github.com>
2021-09-21 21:44:10 +0530

    Update TypeConvertion.py

--- notes/TypeConvertion.py
+++ notes/TypeConvertion.py
@@ -2,10 +2,10 @@ x = 13.949999999999999999 #STEP 1: BY OVERFLOWING THE VALUE
 print(x)
 print(type(x))
 
-g = float("{0:.2f}".format(x)) #STEP 2: CONVERTING THE VALUE WITHOUT CALLING THE ROUND FUNCTION; increase value of 2f to 3f for 3 digits, etc
+g = float("{0:.2f}".format(x)) #STEP 2: CONVERTING THE VALUE USING STR function, increase value of decimal by changing 2f to 3f, 10f, etc(variables not acccepted) -- STR function returns string value
 print(g)
 print(x == g)
 
-h = round(x, 2) #STEP 3: CONVERTING THE VALUE BY CALLING THE ROUND FUNCTION
+h = round(x, 2) #STEP 3: CONVERTING THE VALUE BY CALLING THE ROUND FUNCTION, increase value by changinng ,2 to ,3 or ,n (n is a variable) --returns float value
 print(h)
 print(x == h)
