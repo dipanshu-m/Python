9f3a9a43d4268dc82611eeda1b360b1590ed8a0c
Dipanshu Mahato <38698710+dipanshu-m@users.noreply.github.com>
2021-09-21 22:35:44 +0530

    step 2.5 part
    useful when you need a variable or shit, found it just outta curiosity lol

--- notes/TypeConvertion.py
+++ notes/TypeConvertion.py
@@ -6,6 +6,11 @@ g = float("{0:.2f}".format(x)) #STEP 2: CONVERTING THE VALUE USING STR function,
 print(g)
 print(x == g)
 
+n=2
+i = format(x,f".{n}f" #STEP 2.5: CONVERTING THE VALUE USING STR function, you can use variables here --Returns String type again
+print(i)
+print(float(i) == g)
+
 h = round(x, 2) #STEP 3: CONVERTING THE VALUE BY CALLING THE ROUND FUNCTION, increase value by changinng ,2 to ,3 or ,n (n is a variable) --returns float value
 print(h)
 print(x == h)
