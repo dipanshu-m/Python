900e0ca9378aab1737c97e17a601ae30ad44221b
Dipanshu Mahato <38698710+dipanshu-m@users.noreply.github.com>
2021-09-21 21:44:20 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,18 @@
+print("Welcome to the Tip-Calculator")
+
+bill= input("Enter the total bill: ")
+bill= float(bill)
+
+people = input("Enter the number of people present: ")
+people = int(people)
+
+tax= input("Enter the tax amounted over the bill: ")
+tax = float(tax)
+
+total_bill = bill+bill*tax*0.01
+total_bill= round(total_bill,2)
+print(f"The total bill after considering the taxes are: {total_bill} ")
+
+bill_each=total_bill/people
+bill_each= "{0:.2f}".format(bill_each)
+print(f"\nBill to be paid by {people} people after considering the taxes is : {bill_each} each")
