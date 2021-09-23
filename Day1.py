d259a547c9b2e8ce05cfde3fbc8aedff60e54d6c
Dipanshu Mahato <38698710+dipanshu-m@users.noreply.github.com>
2021-09-23 22:01:41 +0530

    Band Name Generator

--- Day1.py
+++ Day1.py
@@ -3,5 +3,5 @@
 print('WELCOME TO THE BAND NAME GENERATOR')
 home_city= input("Enter the city you grew up on:\n")
 pet_name= input("Enter the name of your pet:\n")
-band_name=home_city+" "+pet_name
+band_name=home_city+" "+pet_name #concatenation
 print("The name of your band suggested is: "+band_name)
